b21f7968489799f710f0fb941961547644038183
akhilk49 <akhilkkurian49@gmail.com>
2024-01-13 14:14:56 +0530

    ok

---
+++ hooks-basics/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css"
+import Content from "./component/Content";
+import Context from "./component/Context";
+
+
+
+function App(){
+  return (
+    <>
+    <Context>
+      <Content/>
+    </Context>
+
+ 
+    
+    </>
+  )
+}
+
+export default App;
